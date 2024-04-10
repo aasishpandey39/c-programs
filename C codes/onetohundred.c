d5a7e4506ceaa1f4eb10121fa30c280464234901
Aasish Pandey <155134420+aasishpandey39@users.noreply.github.com>
2024-04-10 21:00:21 +0530

    Add files via upload

---
+++ C codes/onetohundred.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    for(int i=19;i<=190;i++){
+    printf("%d\n",i);
+    }
+    return 0;
+}
---
+++ C codes/push_pop_operation.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+struct stack{
+    int size ;
+    int top;
+    int * arr;//it is a intiger pointer which allocates the memory dinamically
+};
+ 
+int isEmpty(struct stack* ptr){
+    if(ptr->top == -1){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+}
+ 
+int isFull(struct stack* ptr){
+    if(ptr->top == ptr->size - 1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+ 
+void push(struct stack* ptr, int val){
+    if(isFull(ptr)){
+        printf("Stack Overflow! Cannot push %d to the stack\n", val);
+    }
+    else{
+        ptr->top++;
+        ptr->arr[ptr->top] = val;//ptr->arr is a array of structure pointer and [ptr->top] is atop most position.
+    }
+}
+ 
+int pop(struct stack* ptr){
+    if(isEmpty(ptr)){
+        printf("Stack Underflow! Cannot pop from the stack\n");
+        return -1;
+    }
+    else{
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+ 
+int main(){
+    //*sp means stucture pointer which store the adress of structre pointer
+    struct stack *sp = (struct stack *) malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *) malloc(sp->size * sizeof(int));
+    printf("Stack has been created successfully\n");
+    printf("Before pushing, Full: %d\n", isFull(sp));
+    printf("Before pushing, Empty: %d\n", isEmpty(sp));
+ 
+    return 0;
+}

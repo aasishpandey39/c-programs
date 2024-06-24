624ffd96e87686d5e924f94a66c0cd7cd2d2582a
Aasish Pandey <155134420+aasishpandey39@users.noreply.github.com>
2024-06-25 01:18:02 +0530

    Add files via upload

---
+++ firstprogram.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("aman ashish");
+    return 0;
+}
---
+++ primeno.c
@@ -0,0 +1,7 @@
+#inclde<stdio.h>
+int main(){
+   for (int i=2;i<=20;i=i++){
+      printf("%d",i);
+   }
+   return 0;
+}
